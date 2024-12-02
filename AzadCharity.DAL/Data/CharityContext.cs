6dc3882b0dcba6e9a5d08760753b05c9e332ba20
Mahdi Niyazifard <mahdiniyazifard@gmail.com>
2024-12-02 11:05:46 +0330

    initilized db and context configuration

---
+++ AzadCharity.DAL/Data/CharityContext.cs
@@ -0,0 +1,20 @@
+﻿using AzadCharity.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzadCharity.DAL.Data
+{
+    public class CharityContext : DbContext
+    {
+        public CharityContext(DbContextOptions<CharityContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CharityCase> CharityCases { get; set; }
+    }
+}
