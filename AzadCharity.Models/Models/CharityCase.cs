94c1b31891b718392038857134175f5bb6fdfd69
Mahdi Niyazifard <mahdiniyazifard@gmail.com>
2024-12-02 10:29:56 +0330

    creating the project and adding charity case model

---
+++ AzadCharity.Models/Models/CharityCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzadCharity.Models.Models
+{
+    public class CharityCase
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(1000, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Amount needed is required.")]
+        [Range(1, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public decimal AmountNeeded { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Amount collected must be zero or more.")]
+        public decimal AmountCollected { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
